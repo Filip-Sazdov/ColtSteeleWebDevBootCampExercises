6fb541e58c834cd3fec421da371efc0e848b8ced
Filip Sazdov <filip.sazdov@gmail.com>
2020-06-18 21:59:50 -0400

    added logic

--- Score Keeper/scoreKeeper.js
+++ Score Keeper/scoreKeeper.js
@@ -1,11 +1,12 @@
-let numberSelect = document.getElementById("playingToNumber");
-// let playingToNumberParagraph = document.getElementById("playing to number");
+let input = document.getElementById("playingToNumber");
+let playingToNumberParagraph = document.getElementById("playing to number");
 
-// numberSelect.addEventListener("click", function () {
-//   playingToNumberParagraph.innerHTML("5");
-// });
+input.addEventListener("change", function () {
+  playingToNumberParagraph.textContent = input.value;
+});
 let playerOneButton = document.getElementById("player one");
 let playerTwoButton = document.getElementById("player two");
+let resetButton = document.getElementById("reset");
 let scoreOne = document.getElementById("player one score");
 let scoreTwo = document.getElementById("player two score");
 playerOneButton.addEventListener("click", function () {
@@ -15,8 +16,16 @@ playerOneButton.addEventListener("click", function () {
 playerTwoButton.addEventListener("click", function () {
   scoreTwo.textContent = parseInt(scoreTwo.textContent) + 1;
 });
-let resetButton = document.getElementById("reset");
+
 resetButton.addEventListener("click", function () {
   scoreOne.textContent = 0;
   scoreTwo.textContent = 0;
+  playingToNumberParagraph.textContent = 0;
+  input.value = 0;
 });
+if (
+  input.value === scoreOne.textContent ||
+  input.value === scoreTwo.textContent
+) {
+  alert("yay");
+}
