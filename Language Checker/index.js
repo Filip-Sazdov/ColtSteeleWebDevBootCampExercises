cb83380466e39f5353c859bf5cd0cce582b62926
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-12-29 14:45:58 -0500

    Add colors logic to strings.

--- Language Checker/index.js
+++ Language Checker/index.js
@@ -1,12 +1,13 @@
-let langs = require('langs');
-let franc = require('franc');
+const langs = require('langs');
+const franc = require('franc');
+const colors = require('colors');
 
 let input = process.argv[2];
 let lang = franc(input);
 let result = langs.where('3', lang);
 
 if (lang === 'und') {
-	console.error('Sorry, cannot recognise language, please try with longer sample text!');
+	console.error('Sorry, cannot recognise language, please try with longer sample text!'.magenta.bgYellow);
 } else {
-	console.log(`The language provided should be ${result.name}.`);
+	console.log(`The language provided should be: ${result.name}.`.bgCyan.black);
 }
