a0c3cabe743625b4af594d1b556dc5e75c4d015f
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2022-10-13 19:16:54 -0400

    add delete entry option

--- ToDo List basic/toDoBasic.js
+++ ToDo List basic/toDoBasic.js
@@ -4,16 +4,27 @@ window.setTimeout(function () {
   let answer = prompt("What would you like to do?");
   let aggregatorArray = [];
 
-  while (answer !== "quit") {
-    if (answer == "new") {
+  while (answer !== "q") {
+    if (answer == "add") {
       let listItem = prompt("What new item would you like to add?");
       aggregatorArray.push(listItem);
     } else if (answer == "list") {
-      console.log(aggregatorArray);
+
+      console.log('*********');
+      for (let i = 1; i < aggregatorArray.length; i++) {
+        console.log(i, ':', aggregatorArray[i]);
+      }
+      console.log('*********');
+
+    } else if (answer == "del") {
+      let itemToRemove = parseInt(prompt('Which item would you like to remove? Please use index number from list!'))
+
+      aggregatorArray.splice(itemToRemove, 1);
+      console.log('Item removed!');
     } else {
-      console.log(' Please only use the keywords "new", "list", or "quit"!');
+      console.log(' Please only use the keywords "add", "list", or "q"!');
     }
     answer = prompt("What would you like to do next?");
   }
   console.log("You have exited the app.");
-}, 500);
+}, 5000);
